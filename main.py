5de085c2ef4f044fafca332f689543f0bc40f4f4
Aare Sravanthi <sravanthiaare2@gmail.com>
2026-05-25 20:28:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app.config['SECRET_KEY'] = os.environ.get(
 import os
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL",
-    "sqlite:///taskmanager.db"
+   os.environ.get("DATABASE_URL")
 )
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
